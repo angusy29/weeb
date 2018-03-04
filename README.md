1a92203ed4f730bd1fd70c38f920be17ddb8ef8e
Angus Yuen <angus.yuen29@gmail.com>
2018-03-04 16:30:20 +1100

    Added support for Japanese to English, including Kanji, Hiragana, Katakana

--- README.md
+++ README.md
@@ -2,8 +2,24 @@
 
 Google Chrome plugin which suggests possible Japanese words for the selected English word.
 
+Data provided by Jisho API.
+
 ## Example
 <p align="center">
     <img src="Screenshots/example1.png" width="30%"/>
     <img src="Screenshots/example2.png" width="30%"/>
+    <img src="Screenshots/example3.png" width="30%"/>
+    <img src="Screenshots/example4.png" width="30%"/>
 </p>
+
+## Features
+
+<ul>
+    <li>Find Japanese word suggestions from English word</li>
+    <li>Double click on Japanese Kanji to obtain Furigana</li>    
+    <li>Find meaning of Japanese words in Hiragana or Katakana</li>
+</ul>
+
+## How to use
+
+Double click on a word to open dictionary popup.
--- content.js
+++ content.js
@@ -6,13 +6,20 @@ function dismissPopover() {
     $('#definition').contents().remove();
     $('#infoDiv').css('display', 'none');
 }
+
+function isJapanese(str) {
+	for (var i = 0; i < str.length; i++) {
+		if (str.charCodeAt(i) > 127) return true;
+	}
+	return false;
+}
  
 translate = function() {
     var selectedText = window.getSelection().toString();
     console.log(selectedText);
     $('#definition').contents().remove();
 
-    if (selectedText.length <= 1) {
+    if (selectedText.length <= 0) {
         return;
     }
 
@@ -24,13 +31,16 @@ translate = function() {
         $('#infoDiv').css('position', 'absolute');
         $('#infoDiv').css('left', x - 100);
         $('#infoDiv').css('top', y + 15);
-        $('#definition').append('<h3>Fetching data...</h3>');
+        $('#definition').append('<div class="weeb-h3">Fetching data...</div>');
     } else {
         $('#infoDiv').css('display', 'none');
     }
 
-    http.open("GET", url + '"' + selectedText.toLowerCase() + '"');
+    var toLink = isJapanese(selectedText) ? url + selectedText : url + '"' + selectedText.toLowerCase() + '"';
+    http.open("GET", toLink);
     http.send();
+
+    console.log(selectedText.toLowerCase());
 
     http.onload = () => {
         var json = JSON.parse(http.responseText);
@@ -39,38 +49,40 @@ translate = function() {
         if (json.meta.status == 200) {
             $('#definition').contents().remove();
             if (json.data.length == 0) {
-                $('#definition').append('<h3>No Japanese translation</h3>');
+                $('#definition').append('<div class="weeb-h3">No Japanese translation</div>');
             }
 
             for (var i = 0; i < 4; i++) {
-                if (i >= json.data.length) return;
+                if (i >= json.data.length) break;
                 if (i != 0) {
-                    $('#definition').append('<hr>');
+                    $('#definition').append('<hr class="weeb-hr">');
                 }
 
                 if (json.data[i].japanese[0].reading && !json.data[i].japanese[0].word) {
-                    $('#definition').append('<h3>' + json.data[i].japanese[0].reading + '</h3>');                
+                    $('#definition').append('<div class="weeb-h3">' + json.data[i].japanese[0].reading + '</div>');                
                 } else {
-                    $('#definition').append('<h3>' + json.data[i].japanese[0].word + '</h3>');
+                    $('#definition').append('<div class="weeb-h3">' + json.data[i].japanese[0].word + '</div>');
                     
                     if (json.data[i].japanese[0].reading) {
-                        $('#definition').append('<p>' + json.data[i].japanese[0].reading + '</p>');
+                        $('#definition').append('<div class="weeb-paragraph">' + json.data[i].japanese[0].reading + '</div>');
                     }
                 }
 
                 if (json.data[i].senses[0].english_definitions.length > 0) {
-                    $('#definition').append('<h4>Definitions:</h4>');
-                    var toAdd = '<p>'
+                    $('#definition').append('<div class="weeb-h4">Definitions:</div>');
+                    var toAdd = '<div class="weeb-paragraph">'
                     for (var j = 0; j < json.data[i].senses[0].english_definitions.length; j++) {
                         toAdd += j+1 + ') ' + json.data[i].senses[0].english_definitions[j] + '<br>';
 //                            $('#definition').append(j+1 + ') ' + json.data[i].senses[0].english_definitions[j] + '<br>');
                     }
-                    toAdd += '</p>';
+                    toAdd += '</div>';
                     $('#definition').append(toAdd);
                 }
             }
-            $('#definition').append('<hr>');
-            $('#definition').append('<a href=' + definitionURL + '"' + selectedText + '"' + ' target="_blank">More definitions for ' + '"' + selectedText +'"' + '</a>')
+            $('#definition').append('<hr class="weeb-hr">');
+
+            toLink = isJapanese(selectedText) ? definitionURL + selectedText : definitionURL + '"' + selectedText.toLowerCase() + '"';
+            $('#definition').append('<a class="weeb-link" href=' + toLink + ' target="_blank">More definitions for ' + '"' + selectedText +'"' + '</a>')
             
         }
     }
@@ -79,7 +91,7 @@ translate = function() {
 
 //document.onmouseup = createPopover;
 //document.onkeyup = createPopover;
-$('body').append('<div id="infoDiv" class="card"><div id="forblur"></div><div id="definition" class="cardContainer"></div></div>');
+$('body').append('<div id="infoDiv" class="card"><div id="definition" class="cardContainer"></div></div>');
 
 // dismiss popover on click outside of card
 $(document).click(function(e) {
